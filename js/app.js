d2afb117c2581eed06d34f282ebcff43c2d0a758
Mitch Grove <mgrove96@gmail.com>
2016-04-16 15:33:45 -0700

    Added hadouken sounds and ryu cool animate

--- js/app.js
+++ js/app.js
@@ -1,26 +1,55 @@
 $(function() {
 
-$('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-	$('.ryu-ready').show();
+	$('.ryu').mouseenter(function() {
+	    $('.ryu-action').hide();
+		$('.ryu-ready').show();
+		})
+		.mouseleave(function() {
+	    	$('.ryu-action').hide();
+			$('.ryu-still').show();
+	  	})
+	  	.mousedown(function() {
+		    playHadouken();
+		    $('.ryu-action').hide();
+		  	$('.ryu-throwing').show();
+		  	$('.hadouken').finish().show().animate(
+		  		{'left': '1020px'},
+		  		500,
+		  		function() {
+		    		$(this).hide();
+		    		$(this).css('left', '520px');
+		  		}
+			);
+	  	})
+	  	.mouseup(function() {
+		    $('.ryu-action').hide();
+		  	$('.ryu-ready').show();
+	});
+
+
+
+	$(document).keydown(function(e) {
+		if (e.keyCode == 88)
+		{
+			$('.ryu-action').hide();
+			$('.ryu-cool').show();
+		}
 	})
-	.mouseleave(function() {
-    	$('.ryu-ready').hide();
-		$('.ryu-still').show();
-  	})
-  	.mousedown(function() {
-    // play hadouken sound
-    $('.ryu-ready').hide();
-  	$('.ryu-throwing').show();
-  	$('.hadouken').show();
-    // show hadouken and animate it to the right of the screen
-  	})
-  	.mouseup(function() {
-    $('.ryu-throwing').hide();
-  	$('.ryu-ready').show();
-});
+		.keyup(function(e) {
+			if (e.keyCode == 88)
+			{
+				$('.ryu-action').hide();
+				$('.ryu-still').show();
+			}
+		});	
 
 
 
 
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
